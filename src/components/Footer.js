d809ae6986fc35b47f0d473e16732a0fe3c13bc7
Imran <huseynlimran@gmail.com>
2023-03-24 13:01:37 +0400

    data

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,23 +8,37 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="containerl">
-        <div className="row mt-md-5 d-flex align-items-center justify-content-md-evenly justify-content-between"    aria-label=""
-                    data-scroll
-                    data-scroll-direction="vertical"
-                    data-scroll-speed="3">
-          <div className="col-md-2">
-          </div>
+        <div
+          className="row mt-md-5 d-flex align-items-center justify-content-between"
+          data-scroll
+          data-scroll-direction="vertical"
+          data-scroll-speed="3"
+        >
+          <div className="col-md-2"><svg width="44" height="38" viewBox="0 0 44 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M6.338 35.068C6.338 36.54 5.418 37.276 3.578 37.276C1.76867 37.276 0.864 36.54 0.864 35.068V2.454C0.864 0.981999 1.76867 0.245999 3.578 0.245999C5.418 0.245999 6.338 0.981999 6.338 2.454V35.068Z" fill="black"/>
+<path d="M38.138 2.454C38.138 0.981999 39.0427 0.245999 40.852 0.245999C42.692 0.245999 43.612 0.981999 43.612 2.454V35.068C43.612 36.54 42.692 37.276 40.852 37.276C39.0427 37.276 38.138 36.54 38.138 35.068V2.454ZM24.154 35.068C24.154 36.54 23.234 37.276 21.394 37.276C19.5847 37.276 18.68 36.54 18.68 35.068V2.454C18.68 0.981999 19.5847 0.245999 21.394 0.245999C23.234 0.245999 24.154 0.981999 24.154 2.454V35.068ZM34.918 18.002C34.918 19.0753 34.55 19.98 33.814 20.716C33.078 21.452 32.1733 21.82 31.1 21.82C30.0573 21.82 29.168 21.452 28.432 20.716C27.696 19.98 27.328 19.0753 27.328 18.002C27.328 16.9287 27.696 16.024 28.432 15.288C29.168 14.552 30.0573 14.184 31.1 14.184C32.1733 14.184 33.078 14.552 33.814 15.288C34.55 16.024 34.918 16.9287 34.918 18.002Z" fill="black"/>
+</svg>
+
+</div>
           <div className=" col-md-4 col-7 mb-5">
             <h3 className="mb-4">CONTACT</h3>
             <ul className="social">
-              <Link className="d-flex align-items-center socialItems" to="mailto: huseynlimran@gmail.com" id="">
+              <Link
+                className="d-flex align-items-center socialItems"
+                to="mailto: huseynlimran@gmail.com"
+                id=""
+              >
                 <GrMail className="me-2 socialIcon" /> huseynlimran@gmail.com
               </Link>
-              <Link className="d-flex align-items-center socialItems" to="/" id="">
+              <Link
+                className="d-flex align-items-center socialItems"
+                to="/"
+                id=""
+              >
                 <IoPhonePortraitOutline className="me-2 socialIcon" />
                 +994 55 620 62 24
               </Link>
-              <Link 
+              <Link
                 to="https://github.com/Huseynlimran"
                 target="_blank"
                 rel="noreferrer"
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,6 +16,29 @@ const Navigation = () => {
         delay: 3,
       }
     );
+    gsap.fromTo(
+      ".First",
+      { rotate: "150", opacity: 0 },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 2,
+        stagger: 0.3,
+        delay: 3,
+        rotate: 0,
+      }
+    );
+    gsap.fromTo(
+      ".Second",
+      { Y: -100, opacity: 0 },
+      {
+        opacity: 1,
+        duration: 2,
+        stagger: 0.3,
+        delay: 3.5,
+        y: 0,
+      }
+    );
 
     //       // gsap.fromTo(
     //       //   ".webok",
@@ -90,7 +113,22 @@ const Navigation = () => {
     <nav className="navSticky">
       <div className="containerl navcontainer animationnav">
         <Link to="/" className="logo">
-          Imran
+          <svg
+            width="47"
+            height="43"
+            viewBox="0 0 47 43"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path className="First"
+              d="M6.556 39.816C6.556 41.48 5.516 42.312 3.436 42.312C1.39067 42.312 0.368 41.48 0.368 39.816V2.948C0.368 1.284 1.39067 0.451999 3.436 0.451999C5.516 0.451999 6.556 1.284 6.556 2.948V39.816Z"
+              fill="#CFF306"
+            />
+            <path className="Second"
+              d="M40.156 2.948C40.156 1.284 41.1787 0.451999 43.224 0.451999C45.304 0.451999 46.344 1.284 46.344 2.948V39.816C46.344 41.48 45.304 42.312 43.224 42.312C41.1787 42.312 40.156 41.48 40.156 39.816V2.948ZM24.348 39.816C24.348 41.48 23.308 42.312 21.228 42.312C19.1827 42.312 18.16 41.48 18.16 39.816V2.948C18.16 1.284 19.1827 0.451999 21.228 0.451999C23.308 0.451999 24.348 1.284 24.348 2.948V39.816ZM36.516 20.524C36.516 21.7373 36.1 22.76 35.268 23.592C34.436 24.424 33.4133 24.84 32.2 24.84C31.0213 24.84 30.016 24.424 29.184 23.592C28.352 22.76 27.936 21.7373 27.936 20.524C27.936 19.3107 28.352 18.288 29.184 17.456C30.016 16.624 31.0213 16.208 32.2 16.208C33.4133 16.208 34.436 16.624 35.268 17.456C36.1 18.288 36.516 19.3107 36.516 20.524Z"
+              fill="#CFF306"
+            />
+          </svg>
         </Link>
 
         <ul className={active}>
@@ -107,7 +145,7 @@ const Navigation = () => {
           </li>
           <li className="listLi">
             <a
-            href="myskilss"
+              href="myskilss"
               onClick={closeNav}
               data-scroll-to
               className="linksNav"
